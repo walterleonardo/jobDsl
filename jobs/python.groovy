be2d655fcf6070635f1deb322748e8e89f16da41
Walii server git <walterleonardo@gmail.com>
2019-12-09 10:56:00 +0100

    New groovy

---
+++ jobs/python.groovy
@@ -0,0 +1,24 @@
+project_name = "JobDslName"
+repo = "git@github.com:walterleonardo/jobDsl.git"
+repo_name = "jobDslName"
+
+
+pipelineJob(project_name) {
+	definition {
+		triggers{
+			scm('H/1 * * * *')
+		}
+		
+		cpsScm{
+			scm{
+				git{
+					remote {
+					name(repo_name)
+					url(repo)
+					}
+				}
+				scriptPath("Jenkinsfile")
+			}
+		}
+	}
+}
